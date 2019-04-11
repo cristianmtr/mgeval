92d025ebd35eac6101c187608ac2f1b97a3a9385
cristian <cristi.mtr@gmail.com>
2019-04-11 11:32:07 +0200

    add more

--- example.bat
+++ example.bat
@@ -1,1 +1,3 @@
-python compare.py D:\data\folkdataset\4_mono folk D:\data\hooktheory_dataset\4_mono hook datasets_folk_vs_hook --mid_pattern=*.mid
+REM python compare.py D:\data\folkdataset\4_mono folk D:\data\hooktheory_dataset\4_mono hook datasets_folk_vs_hook --mid_pattern=*.mid
+
+REM python compare.py D:\thesis_code\model_folk100k_melody_bi2lstm32_attention\samples\lamb_transposed\ melody D:\thesis_code\model_folk100k_pianoroll_bi2lstm32_attention\samples\lamb_transposed pianoroll melody_vs_pianoroll
